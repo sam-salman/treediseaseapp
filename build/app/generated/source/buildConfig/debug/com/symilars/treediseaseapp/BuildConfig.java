e7825dde3047a12a642bf0e15e7f716627ce9a0c
chrisbray739 <chrisbray739@gmail.com>
2024-05-18 09:19:32 +0000

    added build folder

---
+++ build/app/generated/source/buildConfig/debug/com/symilars/treediseaseapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.symilars.treediseaseapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.symilars.treediseaseapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
